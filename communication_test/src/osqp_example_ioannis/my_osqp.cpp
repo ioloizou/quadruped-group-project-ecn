1d436245c2954aeedf3598e583ace4b1f5320dae
ioloizou <yiannisloizou@gmail.com>
2024-05-23 12:52:14 +0300

    declared the member functions of mpc class

--- communication_test/src/osqp_example_ioannis/my_osqp.cpp
+++ communication_test/src/osqp_example_ioannis/my_osqp.cpp
@@ -1,32 +1,76 @@
-// osqp-eigen
 #include "OsqpEigen/OsqpEigen.h"
-
-// eigen
 #include <Eigen/Dense>
 
+#include <cmath>
 #include <iostream>
+    
+class MPC{
+public:
+    
+    /**
+     * @brief Sets the rotation matrix based on the given Euler angles.
+     * The robot’s orientation is expressed as a vector of Z-Y-X Euler angles Θ = [φ θ ψ]ᵀ where ψ is the yaw, θ is the pitch, and φ is the roll.
+     * 
+     * @param euler_angles The Euler angles representing the rotation of the body respect to the inertial frame?
+     */
+    void setRotationMatrix(Eigen::Vector3d euler_angles){
+        double psi = euler_angles(2);
+
+        Eigen::Matrix3d R;
+        // Rotation matrix
+        R << cos(psi), -sin(psi), 0,
+             sin(psi), cos(psi), 0,
+             0, 0, 1;
+    }
+
+    void setQMatrix()
+    {}
+
+    void setRMatrix()
+    {}
+
+    void setAMatrixContinious()
+    {}
+
+    void setAMatrixDiscrete()
+    {}
+
+    void setBMatrixContinious()
+    {}
+
+    void setBMatrixDiscrete()
+    {}
+
+    void setEqualityConstraints()
+    {}
+
+    void setInequalityConstraints()
+    {}
+
+    void setBounds()
+    {}
+
+    void setGradient()
+    {}
+
+    void setHessian()
+    {}
+
+    void setInitialGuess()
+    {}
+
+    void solveQP()
+    {}
+
+    void printResults()
+    {}
+private:
+
+    
+};
+
 
 int main(){
-    void setRotationMatrix();
-    
-    void setQMatrix();
-    void setRMatrix();
-
-    void setAMatrixContinious();
-    void setAMatrixDiscrete();
-    void setBMatrixContinious();
-    void setBMatrixDiscrete();
-
-    void setEqualityConstraints();
-    void setInequalityConstraints();
-
-    void setInitialGuess();
-    void setBounds();
-
-    
-
-    void solveQP();
-    void printResults();
 
 
     return 0;
